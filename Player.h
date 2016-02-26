b527d01efe234c5d6dfdf21094c08475498a4136
jmstrnthl <jonah.m.sternthal.18@dartmouth.edu>
2016-02-25 21:57:23 -0500

    updated team.h need cpp code

--- Player.h
+++ Player.h
@@ -7,7 +7,29 @@
 
 
 class Player {
-
+private:
+    int age;
+    int position; //1-PG,2-SG,3-SF,4-PF,5-C
+    float pOFi; //probabilty of injury
+    int rating;
+    int minutes; //minutes played
+    bool injury;
+    int gamesWithInjleft; //games played with injury
+public:
+    //set age
+    //getage
+    //set position
+    //get position
+    //set probability of injury (based on age/minutes played)
+    //get prob of inj
+    //set rating
+    //get rating
+    //set minutes
+    //add minutes
+    //get minutes
+    //change injury
+    //set games with Injury left
+    //decrease games with injury left
 };
 
 
